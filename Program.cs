23120f2d43e96a153554891926ec16d0bd6e953d
Beatriz <beatriz.giovanna@gmail.com>
2025-04-07 00:51:33 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+decimal salario = 0; const decimal porcentagemfgts = 0.08m;
+
+Console.WriteLine("Olá, digite o salário do seu funcionário.");
+salario = Convert.ToDecimal(Console.ReadLine()!);
+
+decimal valorfgts = salario * porcentagemfgts;
+
+Console.WriteLine($"\nSalário (R$)..:`{salario:N2}");
+Console.Write($"\nFGTS: {valorfgts:C2}");
